7c5e04852e05c8c0106fdaf25a31ec4fa563d281
michel <laurentmichelst@gmail.com>
2018-01-31 11:41:01 +0100

    implement menu in mainActivity

--- app/src/main/java/com/example/michel/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/michel/go4lunch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.michel.go4lunch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1.toolbar add toolbar method
+        this.configureToolbar();
+
     }
+
+    // 1.menu implement menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //2 - Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+
 }
