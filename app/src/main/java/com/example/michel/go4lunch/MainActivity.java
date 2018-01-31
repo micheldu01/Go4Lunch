6239229ce132d181b4f4f288f7f3cbf290c3e339
michel <laurentmichelst@gmail.com>
2018-01-31 11:41:48 +0100

    implement toolbar in mainActivity

--- app/src/main/java/com/example/michel/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/michel/go4lunch/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.michel.go4lunch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         //2 - Inflate the menu and add it to the Toolbar
         getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         return true;
+    }
+
+    // 2.toolbar implement toolbar method
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
 
