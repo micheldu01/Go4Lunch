39f99e7248b94f93af628c34740cd5d83c4a12e3
michel <laurentmichelst@gmail.com>
2018-01-31 16:19:01 +0100

    implement TabLayout in mainActivity java

--- app/src/main/java/com/example/michel/go4lunch/MainActivity.java
+++ app/src/main/java/com/example/michel/go4lunch/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.michel.go4lunch;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         // 1.toolbar add toolbar method
         this.configureToolbar();
 
+        //TabLayout execute tabLayout
+        this.configureViewPagerAndTabs();
+
     }
 
     // 1.menu implement menu
@@ -33,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         // Sets the Toolbar
         setSupportActionBar(toolbar);
+    }
+
+    //1.TabLayout create method for implement ViewPager and TabLayout
+    private void configureViewPagerAndTabs(){
+        //Get ViewPager from layout
+        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        //Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+        // 1 - Get TabLayout from layout
+        TabLayout tabs= (TabLayout)findViewById(R.id.activity_main_tabs);
+        // 2 - Glue TabLayout and ViewPager together
+        tabs.setupWithViewPager(pager);
+        // 3 - Design purpose. Tabs have the same width
+        tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 
     // implement button in toolbar
